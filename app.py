1a67e43862149457b1e8cab7580a9c826b51f7c9
smriti <smriti.154@gmail.com>
2017-05-08 00:10:36 +0530

    api

--- app.py
+++ app.py
@@ -42,7 +42,23 @@ class Todo(Resource):
 # shows a list of all todos, and lets you POST to add new tasks
 class TodoList(Resource):
     def get(self):
-        return TODOS
+        # return TODOS
+        dataset = 'Senior Software Test Engineer Person should have very good hands on experience on Selenium. Person should have some experience on overall Selenium framework as well so that he is aware how the entire test automation process work on top of Selenium. Experience on Protractor test would be a plus. End to end automation experience. Person shoud be flexible to jump to do manual testing on need basis. Person may also need to guide junior manual QA.'
+        import re
+        import nltk
+        nltk.download('stopwords')
+        from nltk.corpus import stopwords
+        from nltk.stem.porter import PorterStemmer
+        corpus = []
+        review = re.sub('[^a-zA-Z]', ' ', dataset)
+        review = review.lower()
+        review = review.split()
+        ps = PorterStemmer()
+        stop = set(stopwords.words('english'))
+        review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+        review = ' '.join(review)
+        corpus.append(review)
+        return corpus
 
     def post(self):
         args = parser.parse_args()
